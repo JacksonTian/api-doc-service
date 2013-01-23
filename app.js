ca758db582ebe1dbfd2e8f4bb01dc67e2231ea15
Jackson Tian <shyvo1987@gmail.com>
2013-01-23 13:50:42 +0800

    Update ignore file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use('/wechat', wechat(config.token, function (req, res, next) {
   var input = req.weixin.Content.trim();
   // 用户添加时候的消息
   if (input === 'Hello2BizUser') {
-    return res.reply({msgType: 'text', content: '谢谢添加Node.js公共帐号:)\n回复Node.js API相关关键词，将会得到相关描述'});
+    return res.reply({msgType: 'text', content: '谢谢添加Node.js公共帐号:)\n回复Node.js API相关关键词，将会得到相关描述。如：module, setTimeout等'});
   }
   if (input.length < 2) {
     return res.reply({msgType: 'text', content: '内容太少，请多输入一点:)'});
@@ -23,19 +23,22 @@ app.use('/wechat', wechat(config.token, function (req, res, next) {
       break;
     case 'MATCHED':
       content = data.result.map(function (item) {
-        var replaced = (item.desc || '').replace(/<p>/ig, '')
-          .replace(/<\/p>/ig, '').replace(/<code>/ig, '').replace(/<\/code>/ig, '')
+        var replaced = (item.desc || '')
+          .replace(/<p>/ig, '').replace(/<\/p>/ig, '')
+          .replace(/<code>/ig, '').replace(/<\/code>/ig, '')
           .replace(/<pre>/ig, '').replace(/<\/pre>/ig, '')
           .replace(/<strong>/ig, '').replace(/<\/strong>/ig, '')
+          .replace(/<ul>/ig, '').replace(/<\/ul>/ig, '')
+          .replace(/<li>/ig, '').replace(/<\/li>/ig, '')
           .replace(/&#39;/ig, "'");
         return item.path + '\n' + item.textRaw + ':\n' + replaced;
       }).join('\n');
       if (data.more && data.more.length) {
-        content += '\n更多相关API：\n' + data.more.join(', ').substring(0, 100) + '...';
+        content += '\n更多相关API：\n' + data.more.join(', ').substring(0, 200) + '...';
       }
       break;
     default:
-      content = '没有找到“' + input + '”相关API';
+      content = '没有找到“' + input + '”相关API。输入模块名，方法名，事件名等都能获取到相关内容。';
       break;
   }
   res.reply({msgType: 'text', content: content.substring(0, 2000)});
