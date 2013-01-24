21c5db7d70f605a9a091f894659ce87258692d39
Jackson Tian <shyvo1987@gmail.com>
2013-01-24 19:43:05 +0800

    Add logs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = connect();
 app.use(connect.query());
 app.use(connect.static(__dirname + '/assets', { maxAge: 86400000 }));
 app.use('/wechat', wechat(config.token, function (req, res, next) {
+  console.log(req.weixin);
   var input = req.weixin.Content.trim();
   // 用户添加时候的消息
   if (input === 'Hello2BizUser') {
@@ -44,6 +45,13 @@ app.use('/wechat', wechat(config.token, function (req, res, next) {
     default:
       content = '没有找到“' + input + '”相关API。输入模块名，方法名，事件名等都能获取到相关内容。';
       break;
+  }
+  var from = req.weixin.FromUserName;
+  if (from === 'oPKu7jgOibOA-De4u8J2RuNKpZRw') {
+    content = '主人你好：\n' + content;
+  }
+  if (from === 'oPKu7jpSY1tD1xoyXtECiM3VXzdU') {
+    content = '女王大人:\n' + content;
   }
   res.reply({msgType: 'text', content: content.substring(0, 2000)});
 }));
